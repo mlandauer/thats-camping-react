295f4055263daaa29d1a26294ee8c165c1a48647
Matthew Landauer <mlandauer@gmail.com>
2016-01-02 19:24:38 +1100

    Add real locations

--- components/CampsiteList.js
+++ components/CampsiteList.js
@@ -6,10 +6,10 @@ module.exports = CampsiteList = React.createClass({
     return (
       <ul className="list-group">
         <li className="list-group-item">
-          <Campsite name="Acacia Flat" park="Blue Mountains NP" distance="11 km N"/>
+          <Campsite name="Acacia Flat" park="Blue Mountains NP" distance="11 km N"       latitude="-33.6149" longitude="150.3553" currentLatitude="33.7125" currentLongitude="150.3119"/>
         </li>
         <li className="list-group-item">
-          <Campsite name="Perrys Lookdown" park="Blue Mountains NP" distance="12 km N"/>
+          <Campsite name="Perrys Lookdown" park="Blue Mountains NP" distance="12 km N"       latitude="-33.59935" longitude="150.34592" currentLatitude="33.7125" currentLongitude="150.3119"/>
         </li>
       </ul>
     )
