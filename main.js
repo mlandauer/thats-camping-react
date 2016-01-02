2bc54c83b6bc6e49aab363fd7b27fc0c338542e9
Matthew Landauer <mlandauer@gmail.com>
2016-01-02 18:53:34 +1100

    Campsite component

--- main.js
+++ main.js
@@ -2,7 +2,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Campsite = React.createClass({
+  render: function() {
+    return (
+      <div className="campsite">
+        <div className="pull-right distance">{this.props.distance}</div>
+        <div className="name">{this.props.name}</div>
+        <div className="park">{this.props.park}</div>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Campsite name="Acacia Flat" park="Blue Mountains NP" distance="11 km N"/>,
   document.getElementById('root')
 );
