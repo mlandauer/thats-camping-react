3a8d1b0766e16a5ac0f812bcb128866eaff6e6e1
Matthew Landauer <mlandauer@gmail.com>
2016-01-02 18:58:01 +1100

    Add another campsite back

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ var CampsiteList = React.createClass({
         <li className="list-group-item">
           <Campsite name="Acacia Flat" park="Blue Mountains NP" distance="11 km N"/>
         </li>
+        <li className="list-group-item">
+          <Campsite name="Perrys Lookdown" park="Blue Mountains NP" distance="12 km N"/>
+        </li>
       </ul>
     )
   }
