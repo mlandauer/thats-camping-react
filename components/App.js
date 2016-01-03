e0cdb1ec989c37ae6e0210eea91eabeea9e35c35
Matthew Landauer <mlandauer@gmail.com>
2016-01-03 20:38:35 +1100

    Install react-router

--- components/App.js
+++ components/App.js
@@ -2,6 +2,10 @@ var React = require('react');
 var CampsiteList = require('./CampsiteList');
 var CampsiteDetailPage = require('./CampsiteDetailPage');
 
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Link = require('react-router').Link;
+
 module.exports = App = React.createClass({
   render: function() {
     // Can swap between two pages here
