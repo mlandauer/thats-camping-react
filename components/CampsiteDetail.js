86c9f0c33f460036825d945e49a4d5cbe3fd08de
Matthew Landauer <mlandauer@gmail.com>
2016-01-08 19:48:06 +1100

    Add back button on campsite detail page

--- components/CampsiteDetail.js
+++ components/CampsiteDetail.js
@@ -5,11 +5,20 @@ module.exports = CampsiteDetail = React.createClass({
     return {__html: this.props.children};
   },
 
+  // TODO Extract back button into its own component
+  navigateBack: function() {
+    browserHistory.goBack();
+    return false;
+  },
+
   render: function() {
     return (
       <div className="campsite-detail">
         <nav className="navbar navbar-default navbar-fixed-top">
           <div className="container">
+            <button className="btn btn-link navbar-link navbar-text pull-left" onClick={this.navigateBack}>
+              <span className="glyphicon glyphicon-chevron-left back" aria-hidden="true"></span>
+            </button>
             <h1>{this.props.name}</h1>
           </div>
         </nav>
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
-var browserHistory = createBrowserHistory();
+module.exports = browserHistory = createBrowserHistory();
 
 var CampsiteList = require('./components/CampsiteList');
 var CampsiteDetailPage = require('./components/CampsiteDetailPage');
