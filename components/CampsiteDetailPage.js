3867a280d9f065bf187ce9c6940539ec98669bef
Matthew Landauer <mlandauer@gmail.com>
2016-01-09 11:44:32 +1100

    Fix syntax

--- components/CampsiteDetailPage.js
+++ components/CampsiteDetailPage.js
@@ -2,7 +2,7 @@ var React = require('react');
 var CampsiteDetail = require('./CampsiteDetail');
 
 module.exports = CampsiteDetailPage = React.createClass({
-  componentWillMount() {
+  componentWillMount: function() {
     const id = this.props.params.id
     this.setState({campsite: campsites[id]});
   },
