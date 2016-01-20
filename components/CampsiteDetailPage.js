58a09052e9ff40d2b757c34796dde5aed9d98828
Matthew Landauer <mlandauer@gmail.com>
2016-01-20 12:07:08 +1100

    A little more ES2015

--- components/CampsiteDetailPage.js
+++ components/CampsiteDetailPage.js
@@ -1,19 +1,19 @@
-var React = require('react');
-var CampsiteDetail = require('./CampsiteDetail');
+import React from 'react';
+import CampsiteDetail from './CampsiteDetail';
 
-var CampsiteDetailPage = React.createClass({
-  componentWillMount: function() {
+class CampsiteDetailPage extends React.Component {
+  componentWillMount() {
     const id = this.props.params.id
     var campsite = this.props.campsites[id];
     var park = this.props.parks[campsite.park_id];
     this.setState({campsite: campsite, park: park});
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <CampsiteDetail campsite={this.state.campsite} park={this.state.park} userPosition={this.props.userPosition}/>
     )
   }
-});
+}
 
 module.exports = CampsiteDetailPage;
