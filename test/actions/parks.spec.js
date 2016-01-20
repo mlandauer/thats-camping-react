3ef7ce3785ce2c2ff9171d26934c5a3ace84a19a
Matthew Landauer <mlandauer@gmail.com>
2016-01-20 16:53:02 +1100

    Remove pointless test

--- test/actions/parks.spec.js
+++ test/actions/parks.spec.js
@@ -3,10 +3,6 @@ import * as actions from '../../actions/ParkActions'
 import * as types from '../../constants/ActionTypes'
 
 describe('actions', () => {
-  it('should do some basic arithmetic', () => {
-    expect(1+1).toEqual(2)
-  })
-
   it('addPark', () => {
     const park = {
       id: 1,
