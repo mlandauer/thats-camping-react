f16e3b60bca269976b3862d67d374903eaea5240
Matthew Landauer <mlandauer@gmail.com>
2016-01-20 13:07:36 +1100

    Action for adding a single park

--- test/actions/campsites.spec.js
+++ test/actions/campsites.spec.js
@@ -1,7 +1,22 @@
 import expect from 'expect'
+import * as actions from '../../actions/ParkActions'
+import * as types from '../../constants/ActionTypes'
 
 describe('actions', () => {
   it('should do some basic arithmetic', () => {
     expect(1+1).toEqual(2)
+  })
+
+  it('addPark', () => {
+    const park = {
+      id: 1,
+      shortName: "Blue Mountains NP",
+      longName: "Blue Mountains National Park"
+    }
+    const expectedAction = {
+      type: types.ADD_PARK,
+      park
+    }
+    expect(actions.addPark(park)).toEqual(expectedAction)
   })
 })
