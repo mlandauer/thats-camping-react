bec13f636a272b16049df67a97705d3c882270e2
Matthew Landauer <mlandauer@gmail.com>
2016-01-21 16:08:44 +1100

    Extract component for list of campsites

--- components/CampsiteIndexPage.js
+++ components/CampsiteIndexPage.js
@@ -1,52 +1,7 @@
 import React from 'react';
-import Campsite from './Campsite';
-import data from '../data';
-import { Link } from 'react-router';
-import PositionRelationship from '../libs/PositionRelationship';
+import CampsiteList from './CampsiteList'
 
 export default class CampsiteIndexPage extends React.Component {
-  sortCampsitesArrayByDistance(campsites2, position) {
-    if (position == null) {
-      // TODO: Sort campsites by name
-    } else {
-      // Add distance and bearing information
-      var campsites2 = campsites2.map(function(c) {
-        var distance = PositionRelationship.distanceInMetres(c.position, position);
-        var bearing = PositionRelationship.bearingInDegrees(c.position, position);
-        // TODO Do some kind of concat instead
-        return {
-          name: c.name,
-          position: c.position,
-          park_id: c.park_id,
-          id: c.id,
-          distance: distance,
-          bearing: bearing
-        };
-      });
-
-      // Sort campsites by distance
-      var campsites2 = campsites2.sort(function(a, b) {
-        if (a.distance == undefined && b.distance == undefined) {
-          return a.name.localeCompare(b.name);
-        }
-        if (a.distance == undefined) {
-          return 1;
-        }
-        if (b.distance == undefined) {
-          return -1;
-        }
-        if (a.distance > b.distance) {
-          return 1;
-        }
-        if (a.distance < b.distance) {
-          return -1;
-        }
-        return 0;
-      });
-    }
-    return campsites2;
-  }
-
   render() {
     var position = this.props.position;
     var campsites = this.props.campsites;
@@ -57,7 +12,6 @@ export default class CampsiteIndexPage extends React.Component {
     for (var id in campsites) {
       campsitesArray.push(campsites[id]);
     }
-    let campsites2 = this.sortCampsitesArrayByDistance(campsitesArray, position)
 
     return (
       <div className="campsite-list">
@@ -66,17 +20,7 @@ export default class CampsiteIndexPage extends React.Component {
             <h1>Camping near you</h1>
           </div>
         </nav>
-        <ul className="list-group">
-          {
-            campsites2.map(function(campsite) {
-              return (
-                <Link to={"/campsites/" + campsite.id} className="list-group-item" key={campsite.id}>
-                  <Campsite name={campsite.name} park={parks[campsite.park_id].shortName} distance={campsite.distance} bearing={campsite.bearing}/>
-                </Link>
-              )
-            })
-          }
-        </ul>
+        <CampsiteList campsites={campsitesArray} parks={parks} position={position} />
       </div>
     )
   }
---
+++ components/CampsiteList.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Campsite from './Campsite';
+import data from '../data';
+import { Link } from 'react-router';
+import PositionRelationship from '../libs/PositionRelationship';
+
+export default class CampsiteList extends React.Component {
+  sortCampsitesArrayByDistance(campsites, position) {
+    if (position == null) {
+      // TODO: Sort campsites by name
+    } else {
+      // Add distance and bearing information
+      var campsites = campsites.map(function(c) {
+        var distance = PositionRelationship.distanceInMetres(c.position, position);
+        var bearing = PositionRelationship.bearingInDegrees(c.position, position);
+        // TODO Do some kind of concat instead
+        return {
+          name: c.name,
+          position: c.position,
+          park_id: c.park_id,
+          id: c.id,
+          distance: distance,
+          bearing: bearing
+        };
+      });
+
+      // Sort campsites by distance
+      var campsites = campsites.sort(function(a, b) {
+        if (a.distance == undefined && b.distance == undefined) {
+          return a.name.localeCompare(b.name);
+        }
+        if (a.distance == undefined) {
+          return 1;
+        }
+        if (b.distance == undefined) {
+          return -1;
+        }
+        if (a.distance > b.distance) {
+          return 1;
+        }
+        if (a.distance < b.distance) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    return campsites;
+  }
+
+  render() {
+    var position = this.props.position;
+    // Ugh. For the time being campsites is an array and parks is an object
+    var campsitesArray = this.props.campsites;
+    var parks = this.props.parks;
+
+    let sortedCampsites = this.sortCampsitesArrayByDistance(campsitesArray, position)
+
+    return (
+      <ul className="list-group">
+        {
+          sortedCampsites.map(function(campsite) {
+            return (
+              <Link to={"/campsites/" + campsite.id} className="list-group-item" key={campsite.id}>
+                <Campsite name={campsite.name} park={parks[campsite.park_id].shortName} distance={campsite.distance} bearing={campsite.bearing}/>
+              </Link>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+}
