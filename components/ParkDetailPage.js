dd10e9641c034ee31e915985cb57aea861f8052c
Matthew Landauer <mlandauer@gmail.com>
2016-01-23 12:11:09 +1100

    Not needing to pass things down the chain

--- components/ParkDetailPage.js
+++ components/ParkDetailPage.js
@@ -7,10 +7,10 @@ export default class ParkDetailPage extends React.Component {
     if (park == undefined) {
       return (<div></div>)
     }
-    // Really horrible that we're having to pass both park and parks down the chain
+    // Really horrible that we're having to pass campsites down the chain
     // TODO Fix this
     return (
-      <ParkDetail park={park} parks={this.props.parks} campsites={this.props.campsites} position={this.props.position}/>
+      <ParkDetail park={park} campsites={this.props.campsites} position={this.props.position}/>
     )
   }
 }
