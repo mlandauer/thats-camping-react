b659077d8f7028704a29af7f08553971a60c4980
Matthew Landauer <mlandauer@gmail.com>
2016-01-23 13:17:00 +1100

    Move navigation to top level element

--- components/ParkDetailPage.js
+++ components/ParkDetailPage.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ParkDetail from './ParkDetail'
 
+// Ugh
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+var browserHistory = createBrowserHistory();
+
 export default class ParkDetailPage extends React.Component {
+  // TODO Extract back button into its own component
+  navigateBack() {
+    browserHistory.goBack();
+    return false;
+  }
+
   render() {
     let park = this.props.parks[this.props.params.id]
     if (park == undefined) {
@@ -13,7 +23,17 @@ export default class ParkDetailPage extends React.Component {
     park = Object.assign({}, park, {campsites: campsites})
 
     return (
-      <ParkDetail park={park} position={this.props.position}/>
+      <div className="park-detail-page">
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container">
+            <button className="btn btn-link navbar-link navbar-text pull-left" onClick={this.navigateBack}>
+              <span className="glyphicon glyphicon-chevron-left back" aria-hidden="true"></span>
+            </button>
+            <h1>{park.shortName}</h1>
+          </div>
+        </nav>
+        <ParkDetail park={park} position={this.props.position}/>
+      </div>
     )
   }
 }
