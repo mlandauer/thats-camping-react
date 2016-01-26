fad79bf52034d272c86e486610f7408c7e2172c8
Matthew Landauer <mlandauer@gmail.com>
2016-01-26 14:15:44 +1100

    On heroku forward everything to https. Fixes #41

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.get('/api/data.json', function (request, response){
   response.sendFile(path.resolve(__dirname, 'data.json'))
 })
 
+// When running on heroku (which sets the x-forwarded-proto header) forward
+// everything to https
+app.get('*', function(request, response, next){
+  if(request.headers['x-forwarded-proto'] != undefined && request.headers['x-forwarded-proto'] != 'https')
+    response.redirect('https://thatscamping.herokuapp.com' + request.url)
+  else
+    next() // Continue to other routes if we're not redirecting
+})
+
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response){
