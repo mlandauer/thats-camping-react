484a20443bab84bbf1e22c62fb48bc81498c5c09
Matthew Landauer <mlandauer@gmail.com>
2016-02-19 09:03:15 +1100

    Try using heroku environment variable if it is available to get git revision

--- gitify.js
+++ gitify.js
@@ -7,9 +7,17 @@ var git = require('git-rev-sync');
 
 module.exports = function (file) {
     return through(function (buf, enc, next) {
+        // If we're building this on heroku there will be an environment
+        // variable SOURCE_VERSION. First try using that
+        var revision = process.env.SOURCE_VERSION;
+        // If that doesn't work then try using a local git repository
+        if (revision == undefined) {
+          revision = git.long();
+        }
+
         buf = buf.toString('utf8');
-        buf = buf.replace(/__git_revision_short__/g, git.short());
-        buf = buf.replace(/__git_revision__/g, git.long());
+        buf = buf.replace(/__git_revision_short__/g, revision.substring(0, 7));
+        buf = buf.replace(/__git_revision__/g, revision);
         this.push(buf);
         next();
     });
