bb2c31c1e76ab0bf7880853b77d0c41c473d6d23
Matthew Landauer <mlandauer@gmail.com>
2016-02-21 16:53:54 +1100

    Really crude reload

--- main.js
+++ main.js
@@ -45,6 +45,22 @@ const createStoreWithMiddleware = applyMiddleware(
 
 let store = createStoreWithMiddleware(reducer)
 
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={historyWithScroll}>
