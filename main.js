9fb7ea435c3d784bdbc76a91aa1f425d423262bd
Matthew Landauer <mlandauer@gmail.com>
2016-03-06 08:58:34 +1100

    Don't ask to reload the page

--- main.js
+++ main.js
@@ -61,9 +61,7 @@ window.addEventListener('load', function(e) {
   window.applicationCache.addEventListener('updateready', function(e) {
     if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
       // Browser downloaded a new app cache.
-      if (confirm('A new version of this site is available. Load it?')) {
-        window.location.reload();
-      }
+      window.location.reload();
     } else {
       // Manifest didn't changed. Nothing new to server.
     }
