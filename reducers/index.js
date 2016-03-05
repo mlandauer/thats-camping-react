8dc4429ee46249408b99a79ecec6945da93e3c9b
Matthew Landauer <mlandauer@gmail.com>
2016-03-05 16:18:09 +1100

    Now correctly updates the store

--- reducers/index.js
+++ reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import campsites from './campsites'
 import parks from './parks'
 import position from './position'
+import starred from './starred'
 
 export default combineReducers({
   campsites,
   parks,
-  position
+  position,
+  starred
 })
