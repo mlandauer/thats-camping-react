faf15007b0f002db4d5138d7b7930e98ae4e57bf
Matthew Landauer <mlandauer@gmail.com>
2016-03-30 21:17:12 +1100

    Move calculation of access and facilities fields to component

--- actions/CampsitesActions.js
+++ actions/CampsitesActions.js
@@ -53,96 +53,18 @@ function transformDataToCampsites(data) {
       position: { lat: lat, lng: lng },
       description: c.description,
       park_id: c.park,
-      facilities: facilitiesFields(c),
-      access: accessFields(c)
+      facilities: {
+        toilets: c.toilets,
+        picnicTables: c.picnicTables,
+        barbecues: c.barbecues,
+        showers: c.showers,
+        drinkingWater: c.drinkingWater
+      },
+      access: {
+        caravans: c.caravans,
+        trailers: c.trailers,
+        car: c.car
+      }
     }
   })
-}
-
-function accessFields(campsite) {
-  var have = [];
-  var notHave = [];
-
-  var caravans = campsite.caravans;
-  var trailers = campsite.trailers;
-  var car = campsite.car;
-
-  if (caravans) {
-    have.push("caravans");
-  }
-  else {
-    notHave.push("caravans");
-  }
-  if (trailers) {
-    have.push("trailers");
-  }
-  else {
-    notHave.push("trailers");
-  }
-  if (car) {
-    have.push("car camping");
-  }
-  else {
-    notHave.push("car camping");
-  }
-
-  return {have: have, notHave: notHave};
-}
-
-function facilitiesFields(campsite) {
-  var have = [];
-  var notHave = [];
-
-  var toilets = campsite.toilets;
-  var picnicTables = campsite.picnicTables;
-  var barbecues = campsite.barbecues;
-  var showers = campsite.showers;
-  var drinkingWater = campsite.drinkingWater;
-
-  if (toilets == "flush") {
-    have.push("flush toilets");
-  }
-  else if (toilets == "non_flush") {
-    have.push("non-flush toilets");
-  }
-  else if (toilets == "none") {
-    notHave.push("toilets");
-  }
-
-  if (picnicTables) {
-    have.push("picnic tables");
-  }
-  else {
-    notHave.push("picnic tables");
-  }
-
-  // TODO: show whether you need to bring your own firewood elsewhere
-  // Like "You will need to bring firewood (if you want to use the wood BBQs) and drinking water"
-  if(barbecues == "wood" || barbecues == "wood_supplied" || barbecues == "wood_bring_your_own") {
-    have.push("wood BBQs");
-  }
-  else if (barbecues == "gas_electric") {
-    have.push("gas/electric BBQs");
-  }
-  else if (barbecues == "none") {
-    notHave.push("BBQs");
-  }
-
-  if (showers == "hot") {
-    have.push("hot showers");
-  }
-  else if (showers == "cold") {
-    have.push("cold showers");
-  }
-  else if (showers == "none") {
-    notHave.push("showers");
-  }
-
-  if (drinkingWater) {
-    have.push("drinking water");
-  }
-  else {
-    notHave.push("drinking water");
-  }
-  return {have: have, notHave: notHave};
 }
--- components/CampsiteDetail.js
+++ components/CampsiteDetail.js
@@ -9,8 +9,9 @@ export default class CampsiteDetail extends React.Component {
   }
 
   facilitiesText() {
-    var have = this.props.campsite.facilities.have;
-    var notHave = this.props.campsite.facilities.notHave;
+    var fields = this.facilitiesFields(this.props.campsite.facilities)
+    var have = fields.have;
+    var notHave = fields.notHave;
 
     var r = "";
     if (have.length > 0) {
@@ -28,8 +29,9 @@ export default class CampsiteDetail extends React.Component {
   }
 
   accessText() {
-    var have = this.props.campsite.access.have;
-    var notHave = this.props.campsite.access.notHave;
+    var fields = this.accessFields(this.props.campsite.access)
+    var have = fields.have;
+    var notHave = fields.notHave;
 
     var r = "";
     if (have.length > 0) {
@@ -44,6 +46,94 @@ export default class CampsiteDetail extends React.Component {
       r = r + this.listAsText(notHave);
     }
     return r;
+  }
+
+  accessFields(campsite) {
+    var have = [];
+    var notHave = [];
+
+    var caravans = campsite.caravans;
+    var trailers = campsite.trailers;
+    var car = campsite.car;
+
+    if (caravans) {
+      have.push("caravans");
+    }
+    else {
+      notHave.push("caravans");
+    }
+    if (trailers) {
+      have.push("trailers");
+    }
+    else {
+      notHave.push("trailers");
+    }
+    if (car) {
+      have.push("car camping");
+    }
+    else {
+      notHave.push("car camping");
+    }
+
+    return {have: have, notHave: notHave};
+  }
+
+  facilitiesFields(campsite) {
+    var have = [];
+    var notHave = [];
+
+    var toilets = campsite.toilets;
+    var picnicTables = campsite.picnicTables;
+    var barbecues = campsite.barbecues;
+    var showers = campsite.showers;
+    var drinkingWater = campsite.drinkingWater;
+
+    if (toilets == "flush") {
+      have.push("flush toilets");
+    }
+    else if (toilets == "non_flush") {
+      have.push("non-flush toilets");
+    }
+    else if (toilets == "none") {
+      notHave.push("toilets");
+    }
+
+    if (picnicTables) {
+      have.push("picnic tables");
+    }
+    else {
+      notHave.push("picnic tables");
+    }
+
+    // TODO: show whether you need to bring your own firewood elsewhere
+    // Like "You will need to bring firewood (if you want to use the wood BBQs) and drinking water"
+    if(barbecues == "wood" || barbecues == "wood_supplied" || barbecues == "wood_bring_your_own") {
+      have.push("wood BBQs");
+    }
+    else if (barbecues == "gas_electric") {
+      have.push("gas/electric BBQs");
+    }
+    else if (barbecues == "none") {
+      notHave.push("BBQs");
+    }
+
+    if (showers == "hot") {
+      have.push("hot showers");
+    }
+    else if (showers == "cold") {
+      have.push("cold showers");
+    }
+    else if (showers == "none") {
+      notHave.push("showers");
+    }
+
+    if (drinkingWater) {
+      have.push("drinking water");
+    }
+    else {
+      notHave.push("drinking water");
+    }
+    return {have: have, notHave: notHave};
   }
 
   mapUrl() {
@@ -91,14 +181,8 @@ CampsiteDetail.propTypes = {
   onStarClick: PropTypes.func.isRequired,
   campsite: PropTypes.shape({
     description: PropTypes.string.isRequired,
-    facilities: PropTypes.shape({
-      have: PropTypes.arrayOf(PropTypes.string).isRequired,
-      notHave: PropTypes.arrayOf(PropTypes.string).isRequired,
-    }).isRequired,
-    access: PropTypes.shape({
-      have: PropTypes.arrayOf(PropTypes.string).isRequired,
-      notHave: PropTypes.arrayOf(PropTypes.string).isRequired
-    }).isRequired,
+    facilities: PropTypes.object.isRequired,
+    access: PropTypes.object.isRequired,
     // TODO If position is not present then position should be null, not position.lat
     position: PropTypes.shape({
       lat: PropTypes.number,
