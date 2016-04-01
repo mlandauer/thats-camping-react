c6dccd93531f9d414f85dd216301d08d2281823f
Matthew Landauer <mlandauer@gmail.com>
2016-04-01 17:19:04 +1100

    A bunch more tests and a bunch of assorted refactoring

--- components/CampsiteDetail.js
+++ components/CampsiteDetail.js
@@ -15,17 +15,15 @@ export default class CampsiteDetail extends React.Component {
 
     var r = "";
     if (have.length > 0) {
-      r = r + "Has " + this.listAsText(have);
+      r = r + "has " + this.listAsText(have);
     }
     if (notHave.length > 0) {
       if (have.length > 0) {
-        r = r + " but no ";
-      } else {
-        r = r + "No ";
+        r = r + " but "
       }
-      r = r + this.listAsText(notHave);
+      r = r + "no " + this.listAsText(notHave);
     }
-    return r;
+    return this.capitaliseFirstLetter(r)
   }
 
   accessText(access) {
@@ -35,64 +33,51 @@ export default class CampsiteDetail extends React.Component {
 
     var r = "";
     if (have.length > 0) {
-      r = r + "For " + this.listAsText(have);
+      r = r + "for " + this.listAsText(have)
     }
     if (notHave.length > 0) {
       if (have.length > 0) {
-        r = r + " but not for ";
-      } else {
-        r = r + "Not for ";
+        r = r + " but "
       }
-      r = r + this.listAsText(notHave);
+      r = r + "not for " + this.listAsText(notHave);
     }
-    return r;
+    return this.capitaliseFirstLetter(r);
+  }
+
+  capitaliseFirstLetter(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1)
+  }
+
+  caravans(caravans) {
+    switch(caravans) {
+      case true: return {"have":    "caravans"}
+      default:   return {"notHave": "caravans"}
+    }
+  }
+
+  trailers(trailers) {
+    switch(trailers) {
+      case true: return {"have":    "trailers"}
+      default:   return {"notHave": "trailers"}
+    }
+  }
+
+  car(car) {
+    switch(car) {
+      case true: return {"have":    "car camping"}
+      default:   return {"notHave": "car camping"}
+    }
   }
 
   accessFields(access) {
-    var have = [];
-    var notHave = [];
-
-    var caravans = access.caravans;
-    var trailers = access.trailers;
-    var car = access.car;
-
-    if (caravans) {
-      have.push("caravans");
-    }
-    else {
-      notHave.push("caravans");
-    }
-    if (trailers) {
-      have.push("trailers");
-    }
-    else {
-      notHave.push("trailers");
-    }
-    if (car) {
-      have.push("car camping");
-    }
-    else {
-      notHave.push("car camping");
-    }
-
-    return {have: have, notHave: notHave};
+    var r = {"have": [], "notHave": []}
+    this.merge(r, this.caravans(access.caravans))
+    this.merge(r, this.trailers(access.trailers))
+    this.merge(r, this.car(access.car))
+    return r
   }
 
-  toilets(toilets) {
-    if (toilets == "flush") {
-      return {"have": "flush toilets"}
-    }
-    else if (toilets == "non_flush") {
-      return {"have": "non-flush toilets"}
-    }
-    else if (toilets == "none") {
-      return {"notHave": "toilets"}
-    }
-    else {
-      return {}
-    }
-  }
-
+  // WARNING changes r
   merge(r, t) {
     // TODO Generalise this
     if (t["have"]) {
@@ -101,67 +86,61 @@ export default class CampsiteDetail extends React.Component {
     if (t["notHave"]) {
       r["notHave"].push(t["notHave"])
     }
+    return r
+  }
+
+  toilets(toilets) {
+    switch(toilets) {
+      case "flush":     return {"have":    "flush toilets"}
+      case "non_flush": return {"have":    "non-flush toilets"}
+      case "none":      return {"notHave": "toilets"}
+      default:          return {}
+    }
   }
 
   picnicTables(picnicTables) {
-    if (picnicTables) {
-      return {"have": "picnic tables"}
-    }
-    else {
-      return {"notHave": "picnic tables"}
+    switch(picnicTables) {
+      case true: return {"have":    "picnic tables"}
+      default:   return {"notHave": "picnic tables"}
     }
   }
 
   barbecues(barbecues) {
-    // TODO: show whether you need to bring your own firewood elsewhere
-    // Like "You will need to bring firewood (if you want to use the wood BBQs) and drinking water"
-    if (barbecues == "wood" || barbecues == "wood_supplied" || barbecues == "wood_bring_your_own") {
-      return {"have": "wood BBQs"}
-    }
-    else if (barbecues == "gas_electric") {
-      return {"have": "gas/electric BBQs"}
-    }
-    else if (barbecues == "none") {
-      return {"notHave": "BBQs"}
-    }
-    else {
-      return {}
+    switch(barbecues) {
+      // TODO: show whether you need to bring your own firewood elsewhere
+      // Like "You will need to bring firewood (if you want to use the wood BBQs) and drinking water"
+      case "wood":
+      case "wood_supplied":
+      case "wood_bring_your_own": return {"have":    "wood BBQs"}
+      case "gas_electric":        return {"have":    "gas/electric BBQs"}
+      case "none":                return {"notHave": "BBQs"}
+      default:                    return {}
     }
   }
 
   showers(showers) {
-    if (showers == "hot") {
-      return {"have": "hot showers"}
-    }
-    else if (showers == "cold") {
-      return {"have": "cold showers"}
-    }
-    else if (showers == "none") {
-      return {"notHave": "showers"}
-    }
-    else {
-      return {}
+    switch(showers) {
+      case "hot":  return {"have":    "hot showers"}
+      case "cold": return {"have":    "cold showers"}
+      case "none": return {"notHave": "showers"}
+      default:     return {}
     }
   }
 
   drinkingWater(drinkingWater) {
-    if (drinkingWater) {
-      return {"have": "drinking water"}
-    }
-    else {
-      return {"notHave": "drinking water"}
+    switch(drinkingWater) {
+      case true: return {"have":    "drinking water"}
+      default:   return {"notHave": "drinking water"}
     }
   }
 
   facilitiesFields(facilities) {
     var r = {"have": [], "notHave": []}
-
     this.merge(r, this.toilets(facilities.toilets))
     this.merge(r, this.picnicTables(facilities.picnicTables))
     this.merge(r, this.barbecues(facilities.barbecues))
     this.merge(r, this.showers(facilities.showers))
     this.merge(r, this.drinkingWater(facilities.drinkingWater))
-
     return r;
   }
 
--- test/components/CampsiteDetail.spec.js
+++ test/components/CampsiteDetail.spec.js
@@ -2,129 +2,150 @@ import expect from 'expect'
 import CampsiteDetail from '../../components/CampsiteDetail'
 
 describe('<CampsiteDetail />', () => {
-  it('accessText', () => {
-    var c = new CampsiteDetail
-    expect(c.accessText({caravans: true, trailers: true, car: true}))
-      .toEqual("For caravans, trailers and car camping")
+  describe("access", () => {
+    it('accessText', () => {
+      var c = new CampsiteDetail
+      expect(c.accessText({caravans: true, trailers: true, car: true}))
+        .toEqual("For caravans, trailers and car camping")
+    })
+
+    it('accessText', () => {
+      var c = new CampsiteDetail
+      expect(c.accessText({caravans: false, trailers: true, car: true}))
+        .toEqual("For trailers and car camping but not for caravans")
+    })
+
+    it('accessText', () => {
+      var c = new CampsiteDetail
+      expect(c.accessText({caravans: false, trailers: true, car: false}))
+        .toEqual("For trailers but not for caravans and car camping")
+    })
+
+    it('accessText', () => {
+      var c = new CampsiteDetail
+      expect(c.accessText({caravans: false, trailers: false, car: false}))
+        .toEqual("Not for caravans, trailers and car camping")
+    })
+
+    it('caravans', () => {
+      var c = new CampsiteDetail
+      expect(c.caravans(true)).toEqual({"have": "caravans"})
+      expect(c.caravans(false)).toEqual({"notHave": "caravans"})
+    })
+
+    it('trailers', () => {
+      var c = new CampsiteDetail
+      expect(c.trailers(true)).toEqual({"have": "trailers"})
+      expect(c.trailers(false)).toEqual({"notHave": "trailers"})
+    })
+
+    it('car', () => {
+      var c = new CampsiteDetail
+      expect(c.car(true)).toEqual({"have": "car camping"})
+      expect(c.car(false)).toEqual({"notHave": "car camping"})
+    })
   })
 
-  it('accessText', () => {
-    var c = new CampsiteDetail
-    expect(c.accessText({caravans: false, trailers: true, car: true}))
-      .toEqual("For trailers and car camping but not for caravans")
+  describe("facilities", () => {
+    it('facilitiesText', () => {
+      var c = new CampsiteDetail
+      expect(c.facilitiesText({toilets: "flush", picnicTables: true, drinkingWater: true}))
+        .toEqual("Has flush toilets, picnic tables and drinking water")
+    })
+
+    it('facilitiesText', () => {
+      var c = new CampsiteDetail
+      expect(c.facilitiesText({toilets: "flush", picnicTables: false, drinkingWater: true}))
+        .toEqual("Has flush toilets and drinking water but no picnic tables")
+    })
+
+    it('facilitiesText', () => {
+      var c = new CampsiteDetail
+      expect(c.facilitiesText({toilets: "flush", picnicTables: false, drinkingWater: false}))
+        .toEqual("Has flush toilets but no picnic tables and drinking water")
+    })
+
+    it('facilitiesText', () => {
+      var c = new CampsiteDetail
+      expect(c.facilitiesText({toilets: "none", picnicTables: false, drinkingWater: false}))
+        .toEqual("No toilets, picnic tables and drinking water")
+    })
+
+    it('toilets', () => {
+      var c = new CampsiteDetail
+      expect(c.toilets("flush")).toEqual({"have": "flush toilets"})
+      expect(c.toilets("non_flush")).toEqual({"have": "non-flush toilets"})
+      expect(c.toilets("none")).toEqual({"notHave": "toilets"})
+      expect(c.toilets(undefined)).toEqual({})
+    })
+
+    it('picnicTables', () => {
+      var c = new CampsiteDetail
+      expect(c.picnicTables(true)).toEqual({"have": "picnic tables"})
+      expect(c.picnicTables(false)).toEqual({"notHave": "picnic tables"})
+    })
+
+    it('barbecues', () => {
+      var c = new CampsiteDetail
+      expect(c.barbecues("wood")).toEqual({"have": "wood BBQs"})
+      expect(c.barbecues("wood_supplied")).toEqual({"have": "wood BBQs"})
+      expect(c.barbecues("wood_bring_your_own")).toEqual({"have": "wood BBQs"})
+      expect(c.barbecues("gas_electric")).toEqual({"have": "gas/electric BBQs"})
+      expect(c.barbecues("none")).toEqual({"notHave": "BBQs"})
+      expect(c.barbecues(undefined)).toEqual({})
+    })
+
+    it('showers', () => {
+      var c = new CampsiteDetail
+      expect(c.showers("hot")).toEqual({"have": "hot showers"})
+      expect(c.showers("cold")).toEqual({"have": "cold showers"})
+      expect(c.showers("none")).toEqual({"notHave": "showers"})
+      expect(c.showers(undefined)).toEqual({})
+    })
+
+    it('drinkingWater', () => {
+      var c = new CampsiteDetail
+      expect(c.drinkingWater(true)).toEqual({"have": "drinking water"})
+      expect(c.drinkingWater(false)).toEqual({"notHave": "drinking water"})
+    })
   })
 
-  it('accessText', () => {
+  it('merge', () => {
     var c = new CampsiteDetail
-    expect(c.accessText({caravans: false, trailers: true, car: false}))
-      .toEqual("For trailers but not for caravans and car camping")
+    var original = {"have": ["foo", "bar"], "notHave": ["squiggle"]}
+    c.merge(original, {})
+    expect(original).toEqual({"have": ["foo", "bar"], "notHave": ["squiggle"]})
   })
 
-  it('accessText', () => {
+  it('merge', () => {
     var c = new CampsiteDetail
-    expect(c.accessText({caravans: false, trailers: false, car: false}))
-      .toEqual("Not for caravans, trailers and car camping")
+    var original = {"have": ["foo", "bar"], "notHave": ["squiggle"]}
+    c.merge(original, {"have": "a"})
+    expect(original).toEqual({"have": ["foo", "bar", "a"], "notHave": ["squiggle"]})
   })
 
-  it('facilitiesText', () => {
+  it('merge', () => {
     var c = new CampsiteDetail
-    expect(c.facilitiesText({toilets: "flush", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has flush toilets, picnic tables and drinking water")
+    var original = {"have": ["foo", "bar"], "notHave": ["squiggle"]}
+    c.merge(original, {"notHave": "b"})
+    expect(original).toEqual({"have": ["foo", "bar"], "notHave": ["squiggle", "b"]})
   })
 
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({toilets: "flush", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has flush toilets, picnic tables and drinking water")
+  describe('capitaliseFirstLetter', () => {
+    it("shouldn't do anything to an empty string", () => {
+      var c = new CampsiteDetail
+      expect(c.capitaliseFirstLetter("")).toEqual("")
+    })
+
+    it("should capitalise the first letter", () => {
+      var c = new CampsiteDetail
+      expect(c.capitaliseFirstLetter("a book")).toEqual("A book")
+    })
+
+    it("shouldn't do anything if the first letter is already capitalised", () => {
+      var c = new CampsiteDetail
+      expect(c.capitaliseFirstLetter("Hello World")).toEqual("Hello World")
+    })
   })
 
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({toilets: "non_flush", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has non-flush toilets, picnic tables and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({toilets: "none", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables and drinking water but no toilets")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({toilets: undefined, picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({picnicTables: false, drinkingWater: true}))
-      .toEqual("Has drinking water but no picnic tables")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({barbecues: "wood", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables, wood BBQs and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({barbecues: "wood_supplied", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables, wood BBQs and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({barbecues: "wood_bring_your_own", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables, wood BBQs and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({barbecues: "gas_electric", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables, gas/electric BBQs and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({barbecues: "none", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables and drinking water but no BBQs")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({showers: "hot", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables, hot showers and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({showers: "cold", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables, cold showers and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({showers: "none", picnicTables: true, drinkingWater: true}))
-      .toEqual("Has picnic tables and drinking water but no showers")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({drinkingWater: true, picnicTables: true}))
-      .toEqual("Has picnic tables and drinking water")
-  })
-
-  it('facilitiesText', () => {
-    var c = new CampsiteDetail
-    expect(c.facilitiesText({drinkingWater: false, picnicTables: true}))
-      .toEqual("Has picnic tables but no drinking water")
-  })
 })
