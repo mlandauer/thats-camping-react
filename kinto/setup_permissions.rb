1b7bc95230cf86d5452911957abe9a0920c9e6cd
Matthew Landauer <mlandauer@gmail.com>
2016-04-15 19:09:21 +1000

    Generalise a little

--- kinto/setup_permissions.rb
+++ kinto/setup_permissions.rb
@@ -10,11 +10,17 @@
 require 'rest-client'
 require 'json'
 
-# puts "Some information about the kinto server:"
-# p JSON.parse(RestClient.get('http://thatscamping-kinto.herokuapp.com/v1/'))
-#
-# puts "All the current buckets:"
-# p JSON.parse(RestClient.get('http://admin:foo@thatscamping-kinto.herokuapp.com/v1/buckets'))
+url = 'http://admin:foo2@thatscamping-kinto.herokuapp.com/v1'
+bucket = "thatscamping3"
 
-puts "Create a bucket called thatscamping:"
-p JSON.parse(RestClient.post('http://admin:foo@thatscamping-kinto.herokuapp.com/v1/buckets', {data: {id: "blog"}}.to_json, content_type: :json))
+# puts "Some information about the kinto server:"
+# p JSON.parse(RestClient.get(url + '/'))
+
+# puts "All the current buckets:"
+# p JSON.parse(RestClient.get(url + '/buckets'))
+
+puts "Create the bucket called #{bucket}:"
+p JSON.parse(RestClient.post(url + '/buckets', {data: {id: bucket}}.to_json, content_type: :json))
+
+# puts "Get the #{bucket} bucket"
+# p JSON.parse(RestClient.get(url + '/buckets/' + bucket))
