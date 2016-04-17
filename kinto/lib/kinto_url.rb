81673f2525dd3b94e406c64c5ca4ff97f5573817
Matthew Landauer <mlandauer@gmail.com>
2016-04-17 15:04:41 +1000

    Extract class for api path

--- kinto/lib/kinto_url.rb
+++ kinto/lib/kinto_url.rb
@@ -1,3 +1,29 @@
+class KintoPath
+  def self.server
+    "/"
+  end
+
+  def self.buckets
+    "/buckets"
+  end
+
+  def self.bucket(bucket_id)
+    "#{buckets}/#{bucket_id}"
+  end
+
+  def self.collections(bucket_id)
+    "#{bucket(bucket_id)}/collections"
+  end
+
+  def self.collection(bucket_id, collection_id)
+    "#{collections(bucket_id)}/#{collection_id}"
+  end
+
+  def self.records(bucket_id, collection_id)
+    "#{collection(bucket_id, collection_id)}/records"
+  end
+end
+
 class KintoURL
   attr_reader :base_url
 
@@ -6,26 +32,26 @@ class KintoURL
   end
 
   def server
-    "#{base_url}/"
+    base_url + KintoPath.server
   end
 
   def buckets
-    "#{base_url}/buckets"
+    base_url + KintoPath.buckets
   end
 
   def bucket(bucket_id)
-    "#{buckets}/#{bucket_id}"
+    base_url + KintoPath.bucket(bucket_id)
   end
 
   def collections(bucket_id)
-    "#{bucket(bucket_id)}/collections"
+    base_url + KintoPath.collections(bucket_id)
   end
 
   def collection(bucket_id, collection_id)
-    "#{collections(bucket_id)}/#{collection_id}"
+    base_url + KintoPath.collection(bucket_id, collection_id)
   end
 
   def records(bucket_id, collection_id)
-    "#{collection(bucket_id, collection_id)}/records"
+    base_url + KintoPath.records(bucket_id, collection_id)
   end
 end
