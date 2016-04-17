ded062b06e1ec14876366feb797bf53255c227d2
Matthew Landauer <mlandauer@gmail.com>
2016-04-17 13:33:30 +1000

    Extract url smarts into class

--- kinto/setup_permissions.rb
+++ kinto/setup_permissions.rb
@@ -10,36 +10,70 @@
 require 'rest-client'
 require 'json'
 
+class KintoURL
+  attr_reader :base_url
+
+  def initialize(base_url)
+    @base_url = base_url
+  end
+
+  def server
+    "#{base_url}/"
+  end
+
+  def buckets
+    "#{base_url}/buckets"
+  end
+
+  def bucket(bucket_id)
+    "#{buckets}/#{bucket_id}"
+  end
+
+  def collections(bucket_id)
+    "#{bucket(bucket_id)}/collections"
+  end
+
+  def collection(bucket_id, collection_id)
+    "#{collections(bucket_id)}/#{collection_id}"
+  end
+
+  def records(bucket_id, collection_id)
+    "#{collection(bucket_id, collection_id)}/records"
+  end
+end
+
 url = 'http://admin:foo2@thatscamping-kinto.herokuapp.com/v1'
 bucket = "thatscamping4"
 
-# puts "Some information about the kinto server:"
-# p JSON.parse(RestClient.get("#{url}/"))
+kinto_url = KintoURL.new(url)
 
-# puts "All the current buckets:"
-# p JSON.parse(RestClient.get("#{url}/buckets"))
+puts "Some information about the kinto server:"
+p JSON.parse(RestClient.get(kinto_url.server))
+
+puts "All the current buckets:"
+p JSON.parse(RestClient.get(kinto_url.buckets))
 
 puts "Create the bucket called #{bucket} and make it readable by everyone:"
-p JSON.parse(RestClient.post("#{url}/buckets",
+p JSON.parse(RestClient.post(kinto_url.buckets,
   {data: {id: bucket}, permissions: {read: ["system.Everyone"]}}.to_json,
   content_type: :json))
 
-# puts "Get the #{bucket} bucket:"
-# p JSON.parse(RestClient.get("#{url}/buckets/#{bucket}"))
+puts "Get the #{bucket} bucket:"
+p JSON.parse(RestClient.get(kinto_url.bucket(bucket)))
 
 puts "Create the collection campsite_versions:"
-p JSON.parse(RestClient.post("#{url}/buckets/#{bucket}/collections",
+p JSON.parse(RestClient.post(kinto_url.collections(bucket),
   {data: {id: "campsite_versions"}}.to_json,
   content_type: :json))
 
 puts "List all the campsite records:"
-p JSON.parse(RestClient.get("#{url}/buckets/#{bucket}/collections/campsite_versions/records"))
+p JSON.parse(RestClient.get(kinto_url.records(bucket, "campsite_versions")))
 
 uuid = SecureRandom.uuid
 puts "Generated a uuid: #{uuid}"
 
 puts "Create the first campsite record:"
-p JSON.parse(RestClient.post("#{url}/buckets/#{bucket}/collections/campsite_versions/records",
+p JSON.parse(RestClient.post(kinto_url.records(bucket, "campsite_versions"),
   {data: {
     id: uuid,
     campsite_id: 1,
