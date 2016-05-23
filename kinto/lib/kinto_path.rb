2e490f625d1fd88f4559270f079b69416f3ac1f1
Matthew Landauer <mlandauer@gmail.com>
2016-05-23 17:05:51 +1000

    Inline class

--- kinto/lib/kinto_path.rb
+++ kinto/lib/kinto_path.rb
@@ -1,21 +1,3 @@
-class KintoPartialPath
-  def self.bucket
-    ""
-  end
-
-  def self.collections
-    "/collections"
-  end
-
-  def self.collection(collection_id)
-    "/collections/#{collection_id}"
-  end
-
-  def self.records(collection_id)
-    "/collections/#{collection_id}/records"
-  end
-end
-
 class KintoPath
   def self.server
     "/"
@@ -34,14 +16,14 @@ class KintoPath
   end
 
   def self.collections(bucket_id)
-    "#{buckets}/#{bucket_id}#{KintoPartialPath.collections}"
+    "#{buckets}/#{bucket_id}/collections"
   end
 
   def self.collection(bucket_id, collection_id)
-    "#{buckets}/#{bucket_id}#{KintoPartialPath.collection(collection_id)}"
+    "#{buckets}/#{bucket_id}/collections/#{collection_id}"
   end
 
   def self.records(bucket_id, collection_id)
-    "#{buckets}/#{bucket_id}#{KintoPartialPath.records(collection_id)}"
+    "#{buckets}/#{bucket_id}/collections/#{collection_id}/records"
   end
 end
