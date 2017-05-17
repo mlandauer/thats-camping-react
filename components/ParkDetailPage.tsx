baa966f965e3aa03b7a791c05629dbaa33329d0a
Matthew Landauer <mlandauer@gmail.com>
2017-05-18 06:38:40 +1000

    move another component across

--- components/ParkDetailPage.tsx
+++ components/ParkDetailPage.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import * as React from 'react'
 import ParkDetail from './ParkDetail'
 import Header from './Header'
 import shortenName from '../libs/shortenName'
+import { Position, Campsite } from '../libs/types'
 
-export default class ParkDetailPage extends React.Component {
+interface Park {
+  name: string;
+  description: string;
+  campsite_ids: number[];
+}
+
+interface ParkDetailPageProps {
+  id: number;
+  campsites: Campsite[];
+  parks: Park[];
+  position: Position;
+}
+
+export default class ParkDetailPage extends React.Component<ParkDetailPageProps, {}> {
   render() {
     // Temporary workaround for the fact that parks and campsites do not get
     // update atomically. Only a problem when this page is loaded as the first page
@@ -15,13 +29,13 @@ export default class ParkDetailPage extends React.Component {
     let campsites = park.campsite_ids.map((campsite_id) => {
       return this.props.campsites[campsite_id]
     })
-    park = Object.assign({}, park, {campsites: campsites})
+    let park2 = Object.assign({}, park, {campsites: campsites})
 
     return (
       <div className="park-detail-page">
-        <Header title={shortenName(park.name)}/>
+        <Header title={shortenName(park2.name)} hideBackButton={false} showAboutButton={false}/>
         <div className="content">
-          <ParkDetail park={park} position={this.props.position}/>
+          <ParkDetail park={park2} position={this.props.position}/>
         </div>
       </div>
     )
