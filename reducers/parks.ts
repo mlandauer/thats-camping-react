879f746bcd1e66d8b0ed1bea165c56213b638300
Matthew Landauer <mlandauer@gmail.com>
2017-05-20 16:18:04 +1000

    Move file over to typescript

--- reducers/parks.ts
+++ reducers/parks.ts
@@ -1,10 +1,21 @@
 import { ADD_PARKS } from '../constants/ActionTypes'
 
-export default function parks(state = {}, action) {
+interface Park {
+  id: number;
+}
+
+interface ParksAction {
+  type: string;
+  parks: Park[];
+}
+
+type ParksState = {[index:number]: Park}
+
+export default function parks(state: ParksState = {}, action: ParksAction): ParksState {
   switch(action.type) {
     case ADD_PARKS:
       // Turn array in parks into hash
-      let p = {}
+      let p: ParksState = {}
       action.parks.forEach((park) => {
         p[park.id] = park
       })
