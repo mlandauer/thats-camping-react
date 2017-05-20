5ae895f144873e72c16444b1679a72122ff187cd
Matthew Landauer <mlandauer@gmail.com>
2017-05-20 11:16:09 +1000

    Move another module to typescript

--- actions/PositionActions.ts
+++ actions/PositionActions.ts
@@ -1,19 +1,28 @@
 import * as types from '../constants/ActionTypes'
 
-export function updatePosition(lat, lng) {
+export function updatePosition(lat: number, lng: number) {
   return {
     type: types.UPDATE_POSITION,
     position: {lat: lat, lng: lng}
   }
 }
 
+interface Coords {
+    latitude: number;
+    longitude: number;
+}
+
+interface Location {
+  coords: Coords;
+}
+
 export function startUpdatePosition() {
-  return dispatch => {
+  return (dispatch: (action: {}) => void) => {
     // TODO Also dispatch something immediately to let the user know something is going on
     let locator = new Promise((resolve, reject) => {
 			navigator.geolocation.getCurrentPosition(resolve, reject, {enableHighAccuracy: true});
 		})
-    locator.then((location) => {
+    locator.then((location: Location) => {
       dispatch(updatePosition(location.coords.latitude, location.coords.longitude))
     })
     .catch((err) => {
