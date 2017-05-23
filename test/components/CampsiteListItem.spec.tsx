2e36a35312d2044d33a92a8d2432d687f5f2e355
Matthew Landauer <mlandauer@gmail.com>
2017-05-23 20:15:22 +1000

    Move another test over to typescript

--- test/components/CampsiteListItem.spec.tsx
+++ test/components/CampsiteListItem.spec.tsx
@@ -1,41 +1,41 @@
-import React from 'react'
+import * as React from 'react'
 import { shallow } from 'enzyme'
-import expect from 'expect'
+import * as expect from 'expect'
 import CampsiteListItem from '../../components/CampsiteListItem'
 
 describe('<CampsiteListItem />', () => {
   it('renders the name of the campsite', () => {
-    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite"/>)
+    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite" parkName="A Park" distance={1} bearing={0} starred={false} />)
     expect(wrapper.contains(<div className="name">A Campsite</div>)).toEqual(true)
   })
 
   it('renders the name of the park', () => {
-    const wrapper = shallow(<CampsiteListItem parkName="A Park"/>)
+    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite" parkName="A Park" distance={1} bearing={0} starred={false} />)
     expect(wrapper.contains(<div className="park">A Park</div>)).toEqual(true)
   })
 
   it('renders the bearing 0 as north', () => {
-    const wrapper = shallow(<CampsiteListItem distance={1} bearing={0}/>)
+    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite" parkName="A Park" distance={1} bearing={0} starred={false} />)
     expect(wrapper.find('.distance').text()).toEqual('1 m N')
   })
 
   it('renders the bearing 90 as east', () => {
-    const wrapper = shallow(<CampsiteListItem distance={1} bearing={90}/>)
+    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite" parkName="A Park" distance={1} bearing={90} starred={false} />)
     expect(wrapper.find('.distance').text()).toEqual('1 m E')
   })
 
   it('renders the bearing 45 as east', () => {
-    const wrapper = shallow(<CampsiteListItem distance={1} bearing={45}/>)
+    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite" parkName="A Park" distance={1} bearing={45} starred={false} />)
     expect(wrapper.find('.distance').text()).toEqual('1 m NE')
   })
 
   it('renders larger distances in km', () => {
-    const wrapper = shallow(<CampsiteListItem distance={2345} bearing={0}/>)
+    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite" parkName="A Park" distance={2345} bearing={0} starred={false} />)
     expect(wrapper.find('.distance').text()).toEqual('2 km N')
   })
 
   it('does not render distance when not given', () => {
-    const wrapper = shallow(<CampsiteListItem/>)
+    const wrapper = shallow(<CampsiteListItem campsiteName="A Campsite" parkName="A Park" distance={undefined} bearing={undefined} starred={false} />)
     expect(wrapper.find('.distance').text()).toEqual(' ')
   })
 
